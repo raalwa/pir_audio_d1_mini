5915f08c7cb49ed3dbe9cf482e48e854b3f5dded
Martina Emmert <tina.emmert@web.de>
2023-08-20 22:45:24 +0200

    update for new files

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,26 @@
 #include "AudioGeneratorWAV.h"
 #include "AudioOutputI2S.h"
 
-#include "ily.h"
-#include "bye.h"
-#include "nerd.h"
-#include "see.h"
-#include "who.h"
-#include "wrst.h"
+// #include "ily.h"
+// #include "bye.h"
+// #include "nerd.h"
+// #include "see.h"
+// #include "who.h"
+// #include "wrst.h"
+
+#include "anton_original.h"
+#include "badbird_original.h"
+#include "bleib_original.h"
+#include "bye_original.h"
+#include "hallo_original.h"
+#include "komm_original.h"
+#include "love_original.h"
+#include "peekaboo_original.h"
+#include "schmeckt_original.h"
+#include "schnaps_original.h"
+#include "seeyou_original.h"
+#include "watchadoin_original.h"
+#include "who_original.h"
 
 #define NONE 0
 #define ERROR 1
@@ -45,7 +59,7 @@ void setup()
 
 void select_random_file()
 {
-    long rand = random(1, 7);
+    long rand = random(1, 14);
 
     if (DEBUG_LEVEL >= DEBUG)
     {
@@ -56,48 +70,143 @@ void select_random_file()
     switch (rand)
     {
     case 1:
-        file = new AudioFileSourcePROGMEM(nerd, sizeof(nerd));
+        file = new AudioFileSourcePROGMEM(anton_original, sizeof(anton_original));
         if (DEBUG_LEVEL >= DEBUG)
         {
-            Serial.printf("Selecting 'Nerd'\n");
+            Serial.printf("Selecting 'anton_original'\n");
         }
         break;
     case 2:
-        file = new AudioFileSourcePROGMEM(bye, sizeof(bye));
+        file = new AudioFileSourcePROGMEM(badbird_original, sizeof(badbird_original));
         if (DEBUG_LEVEL >= DEBUG)
         {
-            Serial.printf("Selecting 'Bye'\n");
+            Serial.printf("Selecting 'badbird_original'\n");
         }
         break;
     case 3:
-        file = new AudioFileSourcePROGMEM(ily, sizeof(ily));
+        file = new AudioFileSourcePROGMEM(bleib_original, sizeof(bleib_original));
         if (DEBUG_LEVEL >= DEBUG)
         {
-            Serial.printf("Selecting 'ILY'\n");
+            Serial.printf("Selecting 'bleib_original'\n");
         }
         break;
     case 4:
-        file = new AudioFileSourcePROGMEM(see, sizeof(see));
+        file = new AudioFileSourcePROGMEM(bye_original, sizeof(bye_original));
         if (DEBUG_LEVEL >= DEBUG)
         {
-            Serial.printf("Selecting 'See'\n");
+            Serial.printf("Selecting 'bye_original'\n");
         }
         break;
     case 5:
-        file = new AudioFileSourcePROGMEM(who, sizeof(who));
+        file = new AudioFileSourcePROGMEM(hallo_original, sizeof(hallo_original));
         if (DEBUG_LEVEL >= DEBUG)
         {
-            Serial.printf("Selecting 'Who'\n");
+            Serial.printf("Selecting 'hallo_original'\n");
         }
         break;
     case 6:
-        file = new AudioFileSourcePROGMEM(wrst, sizeof(wrst));
+        file = new AudioFileSourcePROGMEM(komm_original, sizeof(komm_original));
         if (DEBUG_LEVEL >= DEBUG)
         {
-            Serial.printf("Selecting 'Wrst'\n");
+            Serial.printf("Selecting 'komm_original'\n");
+        }
+        break;
+    case 7:
+        file = new AudioFileSourcePROGMEM(love_original, sizeof(love_original));
+        if (DEBUG_LEVEL >= DEBUG)
+        {
+            Serial.printf("Selecting 'love_original'\n");
+        }
+        break;
+    case 8:
+        file = new AudioFileSourcePROGMEM(peekaboo_original, sizeof(peekaboo_original));
+        if (DEBUG_LEVEL >= DEBUG)
+        {
+            Serial.printf("Selecting 'peekaboo_original'\n");
+        }
+        break;
+    case 9:
+        file = new AudioFileSourcePROGMEM(schmeckt_original, sizeof(schmeckt_original));
+        if (DEBUG_LEVEL >= DEBUG)
+        {
+            Serial.printf("Selecting 'schmeckt_original'\n");
+        }
+        break;
+    case 10:
+        file = new AudioFileSourcePROGMEM(schnaps_original, sizeof(schnaps_original));
+        if (DEBUG_LEVEL >= DEBUG)
+        {
+            Serial.printf("Selecting 'schnaps_original'\n");
+        }
+        break;
+    case 11:
+        file = new AudioFileSourcePROGMEM(seeyou_original, sizeof(seeyou_original));
+        if (DEBUG_LEVEL >= DEBUG)
+        {
+            Serial.printf("Selecting 'seeyou_original'\n");
+        }
+        break;
+    case 12:
+        file = new AudioFileSourcePROGMEM(watchadoin_original, sizeof(watchadoin_original));
+        if (DEBUG_LEVEL >= DEBUG)
+        {
+            Serial.printf("Selecting 'watchadoin_original'\n");
+        }
+        break;
+    case 13:
+        file = new AudioFileSourcePROGMEM(who_original, sizeof(who_original));
+        if (DEBUG_LEVEL >= DEBUG)
+        {
+            Serial.printf("Selecting 'who_original'\n");
         }
         break;
     }
+
+    // switch (rand)
+    // {
+    // case 1:
+    //     file = new AudioFileSourcePROGMEM(nerd, sizeof(nerd));
+    //     if (DEBUG_LEVEL >= DEBUG)
+    //     {
+    //         Serial.printf("Selecting 'Nerd'\n");
+    //     }
+    //     break;
+    // case 2:
+    //     file = new AudioFileSourcePROGMEM(bye, sizeof(bye));
+    //     if (DEBUG_LEVEL >= DEBUG)
+    //     {
+    //         Serial.printf("Selecting 'Bye'\n");
+    //     }
+    //     break;
+    // case 3:
+    //     file = new AudioFileSourcePROGMEM(ily, sizeof(ily));
+    //     if (DEBUG_LEVEL >= DEBUG)
+    //     {
+    //         Serial.printf("Selecting 'ILY'\n");
+    //     }
+    //     break;
+    // case 4:
+    //     file = new AudioFileSourcePROGMEM(see, sizeof(see));
+    //     if (DEBUG_LEVEL >= DEBUG)
+    //     {
+    //         Serial.printf("Selecting 'See'\n");
+    //     }
+    //     break;
+    // case 5:
+    //     file = new AudioFileSourcePROGMEM(who, sizeof(who));
+    //     if (DEBUG_LEVEL >= DEBUG)
+    //     {
+    //         Serial.printf("Selecting 'Who'\n");
+    //     }
+    //     break;
+    // case 6:
+    //     file = new AudioFileSourcePROGMEM(wrst, sizeof(wrst));
+    //     if (DEBUG_LEVEL >= DEBUG)
+    //     {
+    //         Serial.printf("Selecting 'Wrst'\n");
+    //     }
+    //     break;
+    // }
 }
 
 void loop()
